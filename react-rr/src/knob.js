60e6d87da3654fa5c5fd8f62fc98dd7bdf1804fe
Michal Sternal <michal.sternal@sportlight.ai>
2023-11-05 09:44:03 +0000

    Dials

--- react-rr/src/knob.js
+++ react-rr/src/knob.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import imageSrc from './knob.png'; // Tell webpack this JS file uses this image
 
-function Dial() {
-  const altText = 'Kill me'; // Replace with the appropriate alt text
+function Dial(steps) {
+  const altText = 'Dial'; // Replace with the appropriate alt text
+  const [rotation, setRotation] = useState(53);
+  const [counter, setCounter] = useState(0);
+
+  const handleClick = () => {
+    setCounter(counter + 1);
+
+    if ((counter + 1) >= steps.steps) {  
+      setCounter(0);
+    }
+
+    console.log(steps.steps);
+    console.log(`Component Clicked: ${counter}`); // You can replace this with your desired action
+  };
 
   return (
-    <div>
+    <div onClick={handleClick}>
+      <div
+        style={{
+          transform: `rotate(${rotation + (180/steps) * counter})`, // Apply the rotation transform
+        }}
+      ></div>
       <img src={imageSrc} alt={altText} />
     </div>
   );
