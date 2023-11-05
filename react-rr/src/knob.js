10d5b60628d90d72b70958e6aa434ccd27504b7e
Tomas Stuikys <tomas.stui@gmail.com>
2023-11-05 10:07:34 +0000

    Made the Knobs look 3d

--- react-rr/src/knob.js
+++ react-rr/src/knob.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import imageSrc from './knob.png'; // Tell webpack this JS file uses this image
 
-function Dial() {
+function Dial({volume, distortion}) {
   const altText = 'Kill me'; // Replace with the appropriate alt text
 
   return (
     <div>
       <img src={imageSrc} alt={altText} />
+      <h3></h3>
     </div>
   );
 }
