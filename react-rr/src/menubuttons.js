059c7c550c98ebb0b59b528a8c23ee46eca150ff
Tomas Stuikys <tomas.stui@gmail.com>
2023-11-05 11:12:48 +0000

    Added sound effects

--- react-rr/src/menubuttons.js
+++ react-rr/src/menubuttons.js
@@ -1,19 +1,25 @@
+import { click } from '@testing-library/user-event/dist/click';
 import React, { useState } from 'react';
+import audioSrc from './click-effect.mp3';
 
 const MenuButtons = ({ updateIsPlaying }) => {
   const [pause, setPause] = useState(false);
   const [reset, setReset] = useState(false);
+  let clickSound = new Audio(audioSrc);
+  clickSound.play();
 
   const handlePauseClick = () => {
     setPause(true);
     updateIsPlaying(false);
     setReset(false); // Ensure reset is false when pausing
+    clickSound.play();
   };
 
   const handlePlayClick = () => {
     setReset(true);
     updateIsPlaying(true);
     setPause(false); // Ensure pause is false when resetting
+    clickSound.play();
   };
 
   const handleResetClick = () => {
@@ -25,11 +31,13 @@ const MenuButtons = ({ updateIsPlaying }) => {
       setReset(true);
       updateIsPlaying(true);
       setPause(false); // Ensure pause is false when resetting
+      clickSound.play();
     }
   }
 
   const handleVolumeIncrease = () => {
     var audioElement = document.getElementById('mp3Audio');
+    clickSound.play();
 
     if (audioElement) {
       // Increase the volume by 10%
@@ -39,6 +47,7 @@ const MenuButtons = ({ updateIsPlaying }) => {
 
   const handleVolumeDecrease = () => {
     var audioElement = document.getElementById('mp3Audio');
+    clickSound.play();
 
     if (audioElement) {
       // Decrease the volume by 10%
